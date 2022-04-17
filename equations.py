f5f7fda67bba2a3812a99d2d5f4142adf894c43b
Vaishnav <87027522+vaish155@users.noreply.github.com>
2022-04-17 19:43:32 +0530

    Update equations.py

--- equations.py
+++ equations.py
@@ -1,12 +1,3 @@
-
-"""
-Requires:
-- Python 3
-- pip install opencv-python
-- pip install pytesseract
-- download Tesseract-OCR data
-"""
-
 import cv2
 import numpy as np
 import pytesseract
@@ -28,14 +19,7 @@ import pyrebase
 import firebase
 
 config={
-    "apiKey": "AIzaSyAFcaRZCduSRhya710Dc_A7No8Me-EHsno",
-    "authDomain": "arithmenia.firebaseapp.com",
-    "projectId": "arithmenia",
-    "storageBucket": "arithmenia.appspot.com",
-    "messagingSenderId": "897754555141",
-    "appId": "1:897754555141:web:b4c168262756d88f3f0d6c",
-    "measurementId": "G-WDWMFGCX14",
-    "databaseURL": "https://arithmenia-default-rtdb.firebaseio.com/"
+    /<Firebase credential data>
 }
 
 firebase=pyrebase.initialize_app(config)
@@ -67,24 +51,18 @@ def get_string(img_path, tub_kernel):
     gray = cv2.dilate(gray, kernel, iterations=1)
     gray = cv2.erode(gray, kernel, iterations=2)
 
-    # Write image after removed noise
     cv2.imwrite(src_path + "removed_noise.png", gray)
 
-    #  Apply threshold to get image with only black and white
-    # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
     _, gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-    # Write the image after apply opencv to do some ...
     cv2.imwrite(src_path + "thres.png", gray)
 
-    # Recognize text with tesseract for python
     result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
 
     return result
 
 cp = cv2.VideoCapture(1)
 while(True):
-    #time.sleep(5)
     camera, frame = cp.read()
     
     if frame is not None:
@@ -93,8 +71,6 @@ while(True):
     q = cv2.waitKey(1)
     if q==ord("q"):
         cv2.imwrite('feedfin.png',frame)
-        
-
         break
 img = cv2.imread('feedfin.png')
 cv2.imshow("original",img)
@@ -125,30 +101,7 @@ print(result)
 print("----- extract expression -------")
 # Split using space
 data = result.strip().split(" ")
-"""print(data)
-
-print(data[0])"""
-"""if(data.count('=')<1):
-    print("Word")
-    img_object = Image.open(img_path)
-    img_text = pt.image_to_string(img_object)
-    print(img_text)
-    with open("maths.txt", "a") as file_object:    
-        file_object.write("\n"+img_text)
-else:"""
 image_1 = Image.open('image/thres.png')
 im_1 = image_1.convert('RGB')
 im_1.save('fin1.pdf')
 storage.child('fin1.pdf').put('fin1.pdf')
-    
-"""print(data[1])
-print(data[2])
-print(data[3])
-print(data[4])
-
-if is_valid_expression(data[0], data[2], data[1], data[4]):
-    print("Correct")
-else:
-    print("Incorrect")
-
-print("------ Done -------")"""
