26922a23023d22cfa3905b376bcdcddcb7f2afa4
Vaishnav <87027522+vaish155@users.noreply.github.com>
2022-04-17 19:11:37 +0530

    Update subproctest.py

--- subproctest.py
+++ subproctest.py
@@ -10,35 +10,20 @@ import firebase
 from textblob import TextBlob
 
 config={
-    "apiKey": "AIzaSyAFcaRZCduSRhya710Dc_A7No8Me-EHsno",
-    "authDomain": "arithmenia.firebaseapp.com",
-    "projectId": "arithmenia",
-    "storageBucket": "arithmenia.appspot.com",
-    "messagingSenderId": "897754555141",
-    "appId": "1:897754555141:web:b4c168262756d88f3f0d6c",
-    "measurementId": "G-WDWMFGCX14",
-    "databaseURL": "https://arithmenia-default-rtdb.firebaseio.com/"
+   // <Firebase credential data>
 }
 
 firebase=pyrebase.initialize_app(config)
 storage=firebase.storage()
-chemistry='Chemistry.txt'
 
 import time
 
 vid=cv2.VideoCapture(1)
-"""
-t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t)
-timesplit=current_time.split(':')
-cursec=timesplit[2]"""
-
 
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 timesplit=current_time.split(':')
 curhr=timesplit[0]
-#print(curhr)
 with open('timetable.csv', mode ='r')as file: 
       
   # reading the CSV file 
@@ -60,7 +45,6 @@ print(doc)
 
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
-    #time.sleep(0.05)
     data = arduino.readline()
     return data
 
@@ -84,7 +68,6 @@ while True:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         break
-#print("out")
 pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 img_object = Image.open('344.png')
 img_text = pt.image_to_string(img_object)
@@ -92,12 +75,7 @@ print("working")
 print(img_text)
 with open(doc, "a") as file_object:    
     file_object.write("\n"+img_text)
-    #with open(doc, "r") as f:        # Opening the test file with the intention to read
-    """text = file_object.read() """                    # Reading the file
-    """textBlb = (TextBlob(str(img_text)))            # Making our first textblob
-    textCorrected = textBlb.correct()   # Correcting the text
-    print(textCorrected)
-    file_object.write("\n"+textCorrected)"""
+
 storage.child(doc).put(doc)
 
 
